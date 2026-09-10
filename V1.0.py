d8a92245f89c1126897d2e8563ee1cc7e17dc914
Ansari Adeena Maqsood Alam <adeenaansari142@gmail.com>
2026-09-10 13:51:33 +0530

    v1.1

--- V1.0.py
+++ V1.0.py
@@ -2,3 +2,8 @@ def register_freelancer(name):
     return "Freelancer " + name + " registered"
 
 print(register_freelancer("Aisha"))
+
+def search_freelancer(name, skill):
+    return "Searching for " + name + " with skill " + skill
+
+print(search_freelancer("Aisha", "Python"))
